1d6a437f08a87aecdf63618d21c3fcc48ddaa418
gurkiratz <singh.gurkirat2707@gmail.com>
2025-12-25 16:14:48 -0500

    add database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from '@/components/ui/button'
-import Image from 'next/image'
+import prisma from '@/lib/db'
 
-const Page = () => {
+const Page = async () => {
+  const users = await prisma.user.findMany()
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <p>Hello World</p>
+      <pre>{JSON.stringify(users, null, 2)}</pre>
       <Button variant={'outline'}>Click me</Button>
     </div>
   )
