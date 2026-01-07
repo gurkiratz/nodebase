706a44ea6d75f3d6c66c36ec50bb47e314c76712
gurkiratz <singh.gurkirat2707@gmail.com>
2026-01-07 16:42:18 -0500

    add background jobs
    - Updated .gitignore to exclude log files.
    - Refactored next.config.ts for consistency in import style.
    - Added new scripts to package.json for development and logging.
    - Updated pnpm-lock.yaml to include new dependencies for Inngest and mprocs.
    - Modified pnpm-workspace.yaml to include inngest-cli as a built dependency.
    - Enhanced Prisma schema with a new Workflow model.
    - Updated page.tsx to implement workflow creation functionality using React Query.
    - Refactored app router to include getWorkflows and createWorkflow procedures with Inngest integration.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,33 @@
+'use client'
 import { requireAuth } from '@/lib/auth-utils'
 import { caller } from '@/trpc/server'
 import { LogoutButton } from './logout'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { useTRPC } from '@/trpc/client'
+import { Button } from '@/components/ui/button'
 
-const Page = async () => {
-  await requireAuth()
+const Page = () => {
+  const trpc = useTRPC()
+  const queryClient = useQueryClient()
+  const { data } = useQuery(trpc.getWorkflows.queryOptions())
 
-  const data = await caller.getUsers()
-
+  const createWorkflow = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        queryClient.invalidateQueries(trpc.getWorkflows.queryOptions())
+      },
+    })
+  )
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center h-screen gap-y-6">
       <p>Protected Server Component</p>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      {JSON.stringify(data, null, 2)}
+      <Button
+        disabled={createWorkflow.isPending}
+        onClick={() => createWorkflow.mutate()}
+      >
+        Create Workflow
+      </Button>
       <LogoutButton />
       {/* protected server component */}
     </div>
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,12 +1,27 @@
-import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init'
+import { inngest } from '@/inngest/client'
+import { createTRPCRouter, protectedProcedure } from '../init'
 import prisma from '@/lib/db'
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ ctx }) => {
-    return prisma.user.findMany({
-      where: {
-        id: ctx.auth.user.id,
+  // getUsers: protectedProcedure.query(({ ctx }) => {
+  //   return prisma.user.findMany({
+  //     where: {
+  //       id: ctx.auth.user.id,
+  //     },
+  //   })
+  // }),
+  getWorkflows: protectedProcedure.query(({ ctx }) => {
+    return prisma.workflow.findMany()
+  }),
+
+  createWorkflow: protectedProcedure.mutation(async () => {
+    // Fetch the video
+    await inngest.send({
+      name: 'test/hello.world',
+      data: {
+        email: 'gur@mail.com',
       },
     })
+    return { success: true, message: 'Workflow created' }
   }),
 })
 // export type definition of API
