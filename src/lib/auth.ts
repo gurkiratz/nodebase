fc54fad95e8bc9bf5a0bbeaac6f8f1e5290b643d
gurkiratz <singh.gurkirat2707@gmail.com>
2026-01-10 14:27:10 -0500

    implement polar payments w/ better-auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,9 @@
+import { polar, checkout, portal, usage, webhooks } from '@polar-sh/better-auth'
+
 import { betterAuth } from 'better-auth'
 import { prismaAdapter } from 'better-auth/adapters/prisma'
 import prisma from '@/lib/db'
+import { polarClient } from './polar'
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -10,4 +13,23 @@ export const auth = betterAuth({
     enabled: true,
     autoSignIn: true,
   },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: '84650d81-16bb-412c-821c-43a88f7b4426',
+              slug: 'pro', // Custom slug for easy reference in Checkout URL, e.g. /checkout/pro
+            },
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+      ],
+    }),
+  ],
 })
