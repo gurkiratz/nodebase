b8d18f3d4b99b0404f8678b971325eeb06dcf908
gurkiratz <singh.gurkirat2707@gmail.com>
2026-01-14 19:14:05 -0500

    create workflow page

--- src/app/(dashboard)/(editor)/workflows/[workflowId]/page.tsx
+++ src/app/(dashboard)/(editor)/workflows/[workflowId]/page.tsx
@@ -1,16 +1,38 @@
-import { requireAuth } from '@/lib/auth-utils'
+import {
+  Editor,
+  EditorError,
+  EditorLoading,
+} from "@/features/editor/components/editor";
+import { WorkflowsContainer } from "@/features/workflows/components/workflows";
+import { prefetchWorkflow } from "@/features/workflows/server/prefetch";
+import { requireAuth } from "@/lib/auth-utils";
+import { HydrateClient } from "@/trpc/server";
+import { ErrorBoundary } from "react-error-boundary";
+import { Suspense } from "react";
+import EditorHeader from "@/features/editor/components/editor-header";
 
 interface PageProps {
   params: Promise<{
-    workflowId: string
-  }>
+    workflowId: string;
+  }>;
 }
 
 const Page = async ({ params }: PageProps) => {
-  await requireAuth()
+  await requireAuth();
 
-  const { workflowId } = await params
-  return <div>Workflow id: {workflowId}</div>
-}
+  const { workflowId } = await params;
+  prefetchWorkflow(workflowId); // just populating the cache
 
-export default Page
+  return (
+    <HydrateClient>
+      <ErrorBoundary fallback={<EditorError />}>
+        <Suspense fallback={<EditorLoading />}>
+          <EditorHeader workflowId={workflowId} />
+          <main className="flex-1"></main>
+          <Editor workflowId={workflowId} />
+        </Suspense>
+      </ErrorBoundary>
+    </HydrateClient>
+  );
+};
+export default Page;
--- src/features/workflows/hooks/use-workflows.ts
+++ src/features/workflows/hooks/use-workflows.ts
@@ -27,7 +27,7 @@ export const useCreateWorkflow = () => {
         queryClient.invalidateQueries(trpc.workflows.getMany.queryOptions({}));
       },
       onError: (error) => {
-        toast.error(`Failed to creatae workflow: ${error.message}`);
+        toast.error(`Failed to create workflow: ${error.message}`);
       },
     })
   );
@@ -46,6 +46,33 @@ export const useRemoveWorkflow = () => {
         queryClient.invalidateQueries(
           trpc.workflows.getOne.queryFilter({ id: data.id })
         );
+      },
+    })
+  );
+};
+
+// Hook to fetch a single workflow using suspense
+export const useSuspenseWorkflow = (id: string) => {
+  const trpc = useTRPC();
+  return useSuspenseQuery(trpc.workflows.getOne.queryOptions({ id }));
+};
+
+// Hook to update a workflow name
+export const useUpdateWorkflowName = () => {
+  const queryClient = useQueryClient();
+  const trpc = useTRPC();
+
+  return useMutation(
+    trpc.workflows.updateName.mutationOptions({
+      onSuccess: (data) => {
+        toast.success(`Workflow "${data.name}" updated`);
+        queryClient.invalidateQueries(trpc.workflows.getMany.queryOptions({}));
+        queryClient.invalidateQueries(
+          trpc.workflows.getOne.queryFilter({ id: data.id })
+        );
+      },
+      onError: (error) => {
+        toast.error(`Failed to update workflow: ${error.message}`);
       },
     })
   );
